f42fcc2f617baf00b1fadb8d68d8fceb6250419c
Шайхиев Эрик Радикович <shajhiev.erik-r@net.ugatu.su>
2023-04-25 22:33:49 +0500

    неявный метод 1ый гит

--- TRS2/TRS2.cpp
+++ TRS2/TRS2.cpp
@@ -75,8 +75,8 @@ vector<vector<double>> ExplicitSchemeMethod(double tau, double h)
     {
         for (int i = 1; i < n_big - 1; i++)
         {
-            U[j][i] = tau / (h * h) * (U[j - 1][i - 1] - 2 * U[j - 1][i] + U[j - 1][i + 1]) + tau * (T[i] * X[i] * X[i]
-                + T[i] * T[i]) + U[j - 1][i];
+            U[j][i] = tau / (h * h) * (U[j - 1][i - 1] - 2 * U[j - 1][i] + U[j - 1][i + 1]) + tau * (T[j] * X[i] * X[i]
+                + T[j] * T[j]) + U[j - 1][i];
         }
         
     }
@@ -95,6 +95,7 @@ vector<vector<double>> ImplicitSchemeMethod(double tau, double h)
     vector <double> X;
     vector <double> T;
     vector <vector<double>> U; // for U t is the first arg, x is second
+    
     for (int i = 0; i < n_big; i++)
     {
         X.push_back(i * h);
@@ -102,29 +103,33 @@ vector<vector<double>> ImplicitSchemeMethod(double tau, double h)
     }
     for (int m = 0; m < n_big; m++)
     {
-        vector<double> temp;
+        vector<double> temp(n_big, 0.);
         U.push_back(temp);
-        for (int n = 0; n < n_big; n++)
-        {
-            U[m].push_back(0.0);
-        }
     }
     for (int i = 0; i < n_big; i++)
     {
         U[0][i] = phi(X[i]);
     }
-    for (int j = 1; j < n_big; j++)
+    for (int n = 1; n < n_big; n++)
     {
-        for (int i = 1; i < n_big - 1; i++)
+        vector <double> altha(n_big,0), betta(n_big,0);
+        altha[0] = 1; betta[0] = -h * psi0(0);
+        for (int i = 1; i <= n_big - 2; i++)
         {
-            U[j][i] = tau / (h * h) * (U[j - 1][i - 1] - 2 * U[j - 1][i] + U[j - 1][i + 1]) + tau * (T[i] * X[i] * X[i]
-                + T[i] * T[i]) + U[j - 1][i];
+            double a = -tau / (h * h);
+            double b = 1 + 2 * tau / (h * h);
+            double c = -tau / (h * h);
+            double z = U[n][i] + tau * (T[n] * X[i] * X[i] + T[n] * T[n]);
+            altha[i] = -a / (b + c * altha[i - 1]);
+            betta[i] = (z - c * betta[i - 1]) / (b + c * altha[i - 1]);
         }
-
+        U[n][n_big - 1] = ( (3 * T[n] * T[n] / 2 + 2) * h + betta[n_big - 2] ) / (1 + h - altha[n_big - 2]);
+        for (int i = n_big - 2; i >= 0; i--)
+        {
+            U[n][i] = altha[i] * U[n][i + 1] + betta[i];
+        }
     }
-    
-    
-    
+    PrintMatrix(U);
     return U;
 }
 int main()
@@ -133,6 +138,7 @@ int main()
     double h = 0.1; // so then N is 10, and T=tau*N=50 ms
     double tau = 0.005;
     ExplicitSchemeMethod(tau, h);
+    ImplicitSchemeMethod(tau, h);
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
